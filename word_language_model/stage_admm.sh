9916aba2a53728a6e75596a20631be5d2b6922b1
Jiaming Xie <jmxie@pku.edu.cn>
2019-02-20 23:21:42 +0800

    add log

--- word_language_model/stage_admm.sh
+++ word_language_model/stage_admm.sh
@@ -13,17 +13,22 @@ sp=$begin_sp
 log="sp${begin_sp}-${end_sp}_stage_admm.log"
 rm $log &>/dev/null
 
-base="python -u admm_prune.py --cuda --emsize 1500 --nhid 1500 --dropout 0.60 --tied --data ./data/ptb --arch ptb --config_file $config_yaml --admm --rho 0.001 --sparsity_type balanced_row "
-load="ptb_pretrained_1500_tied.pt"
+base="python -u admm_prune.py --cuda --emsize 1500 --nhid 1500 --data ./data/ptb --arch ptb --config_file $config_yaml --admm --rho 0.001 --sparsity_type balanced_row "
+load="ptb_pretrained_1500.pt"
 while [ $sp -le $end_sp ]; do
-  if [ $sp -lt $end_sp ]; then
+  if [ $sp -lt 70 ]; then
     epoch=10
-  else 
+    dropout=0.60
+  elif [ $sp -lt 80 ]; then
+    epoch=20
+    dropout=0.55
+  else
     epoch=40
+    dropout=0.50
   fi
-  save="sp${sp}_epoch_${epoch}_lstm.pt"
+  save="sp${sp}_epoch${epoch}_dropout${dropout}_lstm.pt"
   ExportConfig $sp ${config_yaml} 
-  cmd="$base --epoch $epoch --save $save --pretrained $load"
+  cmd="$base --epoch $epoch --dropout $dropout --save $save --pretrained $load"
   echo "$cmd" | tee -a $log
   $cmd | tee -a $log
   if [ $sp -lt 70 ]; then
@@ -31,7 +36,7 @@ while [ $sp -le $end_sp ]; do
   elif [ $sp -le 80 ]; then
     change=2
   else
-    change=1
+    change=2
   fi
   sp=`expr $sp + $change`
   load=$save
